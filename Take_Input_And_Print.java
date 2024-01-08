74853856a900c0356081829de7f47042f0300426
ssricharmika <119724258+ssricharmika@users.noreply.github.com>
2024-01-08 12:02:28 +0530

    import java.util.Scanner; public class My {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String a=sc.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class My
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.print(a);
+    }
+}
