54942def6022ab1b94602904d1636f86716472be
ssricharmika <119724258+ssricharmika@users.noreply.github.com>
2024-01-11 09:29:30 +0530

    import java.util.Scanner; public class Example{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String a=sc.nextLine();         System.out.println(a);     } }

--- Take_Input_And_Print.java
+++ Take_Input_And_Print.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-public class My
-{
+public class Example{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
         String a=sc.nextLine();
-        System.out.print(a);
+        System.out.println(a);
     }
 }
