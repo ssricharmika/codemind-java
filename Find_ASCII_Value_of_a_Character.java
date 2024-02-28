6dbe1c6a30cfc20bc28c26103c3d7b61302d5e62
srighakollapu sri charmika <119724258+ssricharmika@users.noreply.github.com>
2024-02-28 10:56:05 +0530

    import java.util.*; public class Exam{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         char n=sc.next().charAt(0);         int a=n;         System.out.println(a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Exam{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        char n=sc.next().charAt(0);  
+        int a=n;
+        System.out.println(a);
+    }
+}
